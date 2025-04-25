3bf62dc2a5287369624c557bfe4b582f5db3300c
iamalshahed <shahedevise@gmail.com>
2025-04-26 01:09:39 +0600

    footer, all device responsive is done

--- assets/js/main.js
+++ assets/js/main.js
@@ -58,10 +58,70 @@ let swiperFavorite = new Swiper('.favorite__swiper', {
   })
 
 
-/*=============== SHOW SCROLL UP ===============*/ 
+/*=============== SHOW SCROLL UP ===============*/
+
+const scrollUp = () =>{
+	const scrollUp = document.getElementById('scroll-up')
+    // When the scroll is higher than 350 viewport height, add the show-scroll class to the a tag with the scrollup class
+	this.scrollY >= 350 ? scrollUp.classList.add('show-scroll')
+						: scrollUp.classList.remove('show-scroll')
+}
+window.addEventListener('scroll', scrollUp)
 
 
 /*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
 
+const sections = document.querySelectorAll('section[id]')
+    
+const scrollActive = () =>{
+  	const scrollDown = window.scrollY
+
+	sections.forEach(current =>{
+		const sectionHeight = current.offsetHeight,
+			  sectionTop = current.offsetTop - 58,
+			  sectionId = current.getAttribute('id'),
+			  sectionsClass = document.querySelector('.nav__menu a[href*=' + sectionId + ']')
+
+		if(scrollDown > sectionTop && scrollDown <= sectionTop + sectionHeight){
+			sectionsClass.classList.add('active-link')
+		}else{
+			sectionsClass.classList.remove('active-link')
+		}                                                    
+	})
+}
+window.addEventListener('scroll', scrollActive)
+
 
 /*=============== SCROLL REVEAL ANIMATION ===============*/
+
+const sr = ScrollReveal({
+    origin: 'top',
+    distance: '60px',
+    duration: 2500,
+    delay: 400,
+    reset: true, // Animations repeat
+})
+
+sr.reveal(`.home__social, .favorite__container, .sponsor__container, .footer`)
+
+sr.reveal(`.home__title span:nth-child(1)`, {
+    origin: 'left',
+    opacity: 1
+})
+
+sr.reveal(`.home__title span:nth-child(3)`, {
+    origin: 'right',
+    opacity: 1
+})
+
+sr.reveal(`.home__tooltip, .home__button, .model__button`, {
+    origin: 'bottom',
+})
+
+sr.reveal(`.about__data`, {
+    origin: 'left',
+})
+
+sr.reveal(`.about__img, .model__tooltip`, {
+    origin: 'right',
+})
